773c30151e61508805df476650cc4a7f6f14ad27
yhzdys <yhzdys@foxmail.com>
2023-03-15 18:21:02 +0800

    Clean Code

--- src/main/java/com/yhzdys/litchi/exception/LitchiException.java
+++ src/main/java/com/yhzdys/litchi/exception/LitchiException.java
@@ -2,22 +2,7 @@ package com.yhzdys.litchi.exception;
 
 public class LitchiException extends RuntimeException {
 
-    public LitchiException() {
-    }
-
     public LitchiException(String message) {
         super(message);
-    }
-
-    public LitchiException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public LitchiException(Throwable cause) {
-        super(cause);
-    }
-
-    public LitchiException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/com/yhzdys/litchi/transaction/aspect/LitchiTransactionAdvisor.java
+++ src/main/java/com/yhzdys/litchi/transaction/aspect/LitchiTransactionAdvisor.java
@@ -3,14 +3,11 @@ package com.yhzdys.litchi.transaction.aspect;
 import org.aopalliance.aop.Advice;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractPointcutAdvisor;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
 
 /**
  * spring aop切面定义
  */
-public class LitchiTransactionAdvisor extends AbstractPointcutAdvisor implements BeanFactoryAware {
+public class LitchiTransactionAdvisor extends AbstractPointcutAdvisor {
 
     private final Advice advice = new TransactionalInterceptor();
 
@@ -29,12 +26,5 @@ public class LitchiTransactionAdvisor extends AbstractPointcutAdvisor implements
     @Override
     public Advice getAdvice() {
         return advice;
-    }
-
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        if (advice instanceof BeanFactoryAware) {
-            ((BeanFactoryAware) advice).setBeanFactory(beanFactory);
-        }
     }
 }
